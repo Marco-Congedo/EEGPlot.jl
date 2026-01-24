1df77893b481c1277d36826d2ab3f97a865d19c5
Marco Congedo <marco.congedo@gmail.com>
2026-01-24 08:57:02 +0100

    dev

--- docs/local_run.jl
+++ docs/local_run.jl
@@ -1,6 +1,7 @@
 # This script allows to visualize the documentation once make.jl has executed.
 # LiveServer.jl must be installed in julia default environment.
 
+
 println("Done.\n")
 @info "To see the documentation using LiveServer, CTRL+click on the link here below:"
 println("")
